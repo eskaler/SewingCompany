eeb38b87d233409dabadaf1a9f7f5d82093d6c94
Алексей Лаптев <radio.volchanka@gmail.com>
2019-05-07 13:10:42 +0500

    2

--- SewingCompany/Pages/PgProductConstructor.xaml.cs
+++ SewingCompany/Pages/PgProductConstructor.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -66,6 +67,43 @@ namespace SewingCompany.Pages
             catch
             {
                 ImFurnitureView.Source = new BitmapImage(new Uri(@".\..\Resources\Images\System\no-image.jpg", UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        RotateTransform FurnitureRotation = new RotateTransform(0);
+
+        private void BtnRotateLeft_Click(object sender, RoutedEventArgs e)
+        {
+            FurnitureRotation.Angle -= 10;
+            ImFurnitureView.RenderTransform = FurnitureRotation;
+        }
+
+        private void BtnRotateRight_Click(object sender, RoutedEventArgs e)
+        {
+            FurnitureRotation.Angle += 10;
+            ImFurnitureView.RenderTransform = FurnitureRotation;
+        }
+
+        private void BtnAddCustomFabric_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Выберите изображение";
+            op.Filter = "Все поддерживаемые форматы|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                if (sender.Equals(BtnAddCustomFabric))
+                {
+                    ImFabricView.Source = new BitmapImage(new Uri(op.FileName));
+                    TbCustomFabricPath.Text = op.FileName;
+                }
+                else
+                {
+                    ImFurnitureView.Source = new BitmapImage(new Uri(op.FileName));
+                    TbCustomFurniturePath.Text = op.FileName;
+                }
+                
             }
         }
     }
